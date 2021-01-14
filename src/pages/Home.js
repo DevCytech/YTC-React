e2c43fea4d4be1f972589e89c27324a993f1079d
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-01-13 23:47:58 -0500

    Updating Page Title
    Making sure the document title is always set to the title of the current page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/Utilities.css';
 import '../styles/Common.css';
 
 export default function PageNotFound() {
+	useEffect(() => {
+		document.title = 'The YouTube Community';
+	});
+	
 	return (
 		<>
 			<Header />
